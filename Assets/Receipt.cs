176d9653dc99e66e00c908e27efa4ce07cff50c9
DongHwan <euen1995@naver.com>
2022-10-13 13:18:04 +0900

    PlayNANOO

--- Assets/Receipt.cs
+++ Assets/Receipt.cs
@@ -12,7 +12,41 @@ public class Receipt : MonoBehaviour
     {
         plugin = Plugin.GetInstance();
     }
-
+    
+    public void LoginGuest()
+    {
+        plugin.AccountGuestSignIn((status, errorCode, jsonString, values) => {
+            if (status.Equals(Configure.PN_API_STATE_SUCCESS))
+            {
+                Debug.Log(values["access_token"].ToString());
+                Debug.Log(values["refresh_token"].ToString());
+                Debug.Log(values["uuid"].ToString());
+                Debug.Log(values["openID"].ToString());
+                Debug.Log(values["nickname"].ToString());
+                Debug.Log(values["linkedID"].ToString());
+                Debug.Log(values["linkedType"].ToString());
+                Debug.Log(values["country"].ToString());
+            }
+            else
+            {
+                if (values != null)
+                {
+                    if (values["ErrorCode"].ToString() == "30007")
+                    {
+                        Debug.Log(values["WithdrawalKey"].ToString());
+                    }
+                    else
+                    {
+                        Debug.Log("Fail");
+                    }
+                }
+                else
+                {
+                    Debug.Log("Fail");
+                }
+            }
+        });
+    }
 
     public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
     {
